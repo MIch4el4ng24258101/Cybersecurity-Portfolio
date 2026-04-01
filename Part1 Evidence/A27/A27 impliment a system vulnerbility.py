732eab07097d489004fd388c436e954ee4b70921
MIch4el4ng24258101 <24258101@student.uwa.edu.au>
2026-04-01 17:41:01 +0800

    Delete Part1 Evidence/A27/A27 impliment a system vulnerbility.py

--- Part1 Evidence/A27/A27 impliment a system vulnerbility.py
+++
@@ -1,5 +0,0 @@
-username = input("Enter username: ")
-password = input("Enter password: ")
-
-query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'"
-print(query)
